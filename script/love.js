fe410038d0c30ee7b1a933603d6cac8c6a5e091c
Ryusenpai <75114719+ryusenpai21@users.noreply.github.com>
2025-02-22 22:06:54 +0700

    Update love.js

--- script/love.js
+++ script/love.js
@@ -39,7 +39,7 @@ function init() {
     ctx.font = "60px Arial";
     ctx.fillStyle = "lightblue";
     ctx.textAlign = "center";
-    ctx.fillText("I love you sayang", width / 2, height / 2.2 + 400);
+    ctx.fillText("I love you fa ><", width / 2, height / 2.2 + 400);
   }
 
   function heartPosition(rad) {
